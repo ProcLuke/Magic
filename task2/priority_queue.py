ee9491d8f8243f301a336169dbc4529e431fbe4b
Lukas Prochazka <pro41216@spseol.cz>
2024-02-07 14:08:41 +0100

    2

--- task2/priority_queue.py
+++ task2/priority_queue.py
@@ -1,9 +1,27 @@
 class PriorityQueue:
-    def push(self):
-        pass
+    def __init__(self):
+        self.Queue = []
+        
+    def __len__(self):
+         return len(self.Queue)
+    
+    def push(self, task, priority):
+        self.Queue.append((task, priority))
+        self.Queue.sort(key=lambda x: x[1])
 
+    def __iter__(self):
+        self.position = 0
+        return self
+    
+    def __next__(self):
+        if self.position < len(self.Queue):
+            queue = self.Queue[self.position]
+            self.position += 1
+            return queue
+        else:
+            raise StopIteration
 
-
+"""
 priority_queue = PriorityQueue()
 priority_queue.push("Task 1", 3)
 priority_queue.push("Task 2", 1)
@@ -19,3 +37,4 @@ print("Processing tasks in Priority Order:")
 while len(priority_queue) > 0:
     task = priority_queue.pop()
     print("Processing:", task)
+"""
