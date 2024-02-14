78954c413c005540e43d30b345506c70685b6634
Lukas Prochazka <pro41216@spseol.cz>
2024-02-14 14:10:27 +0100

    4

--- task3/linked_list.py
+++ task3/linked_list.py
@@ -5,9 +5,33 @@ class Node:
         self.prev_node = None
 
 
-class DoublyLinkedList():
+class DoublyLinkedList:
     def __init__(self):
         super().__init__()
         self.head = None
         self.tail = None
         self.length = 0
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            new_node.prev_node = new_node
+            self.tail = new_node
+        self.length +=1
+
+    def __len__(self):
+        return self.length
+    
+    def __getitem__(self, index:int):
+        if isinstance(index,int):
+            raise TypeError
+        node = self.head
+        for _ in range(index):
+            node = node.next_node
+        return node.data
+
+
